61e444fb87e460d6261a4e494cd3e802fde1ab3c
Leon <liang.li@charleskeith.com>
2018-06-27 08:46:37 +0800

    http client test

--- lib/app.dart
+++ lib/app.dart
@@ -4,6 +4,7 @@ import 'home.dart';
 import 'contact.dart';
 import 'discover.dart';
 import 'me.dart';
+import 'package:flutter_app/ckc/ckchome.dart';
 
 class AppMain extends StatefulWidget {
   AppMain({Key key, this.title}) : super(key: key);
@@ -18,24 +19,29 @@ class AppMain extends StatefulWidget {
 class AppMainState extends State<AppMain> {
 
   final List<BottomNavigationBarItem> listSet =
-  [new BottomNavigationBarItem(
-    icon: new Icon(Icons.chat,color: Colors.grey),
-    title: new Text('微信'),
-  ),
-  new BottomNavigationBarItem(
-    icon: new Icon(Icons.perm_contact_calendar,color: Colors.grey,),
-    title: new Text('通讯录'),
-  ),
-  new BottomNavigationBarItem(
-    icon: new Icon(Icons.room,color: Colors.grey),
-    title: new Text('发现'),
-  ),
-  new BottomNavigationBarItem(
-    icon: new Icon(Icons.perm_identity,color: Colors.grey),
-    title: new Text('我'),
-  ),
+  [
+    new BottomNavigationBarItem(
+      icon: new Icon(Icons.chat,color: Colors.grey),
+      title: new Text('微信'),
+    ),
+    new BottomNavigationBarItem(
+      icon: new Icon(Icons.perm_contact_calendar,color: Colors.grey,),
+      title: new Text('通讯录'),
+    ),
+    new BottomNavigationBarItem(
+      icon: new Icon(Icons.room,color: Colors.grey),
+      title: new Text('发现'),
+    ),
+    new BottomNavigationBarItem(
+      icon: new Icon(Icons.perm_identity,color: Colors.grey),
+      title: new Text('我'),
+    ),
+    new BottomNavigationBarItem(
+      icon: new Icon(Icons.home,color: Colors.grey),
+      title: new Text('首页'),
+    ),
   ];
-  final List<StatefulWidget> vcSet = [new AppHome(),new AppContact(),new AppDiscover(),new AppMe()];
+  final List<StatefulWidget> vcSet = [new AppHome(),new AppContact(),new AppDiscover(),new AppMe(), new CKCHome()];
   int _sindex=0;
   @override
   Widget build(BuildContext context){
---
+++ lib/ckc/ckchome.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'dart:io';
+import 'dart:convert';
+
+class CKCHome extends StatefulWidget {
+  CKCHome({Key key, this.title}) : super(key: key);
+  final String title;
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return CKCHomeState();
+  }
+}
+
+class CKCHomeState extends State<CKCHome> {
+  
+  var IPAddress = 'Unknown';
+  getIPAddress() async {
+    var url = 'https://httpbin.org/ip';
+    var httpClient = new HttpClient();
+
+    String result;
+    try {
+      var request = await httpClient.getUrl(Uri.parse(url));
+      var response = await request.close();
+      if(response.statusCode == HttpStatus.OK) {
+        var json = await response.transform(UTF8.decoder).join();
+        var data = JSON.decode(json);
+        result = data['origin'];
+      } else {
+        result = 'Error getting IP address:\nHttp status ${response.statusCode}';
+      }
+    } catch (exception) {
+      result = 'Failed getting IP address';
+    }
+    if(!mounted) {
+      return;
+    }
+    setState(() {
+      IPAddress = result;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return new MaterialApp(
+      theme: new ThemeData(
+        primaryColor: Colors.grey[100],
+        primaryColorBrightness: Brightness.light,
+      ),
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text('首页'),
+          backgroundColor: Colors.grey[200],
+        ),
+        body: new Center(
+          child: new Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              new Text('Your current IP address is:'),
+              new Text('$IPAddress'),
+              new SizedBox(height: 32.0),
+              new RaisedButton(
+                  onPressed: getIPAddress,
+                  child: new Text('Get IP address'),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
