5d78495c8646ac38b149c108fe266b2fe8e6ff1c
Leon <liang.li@charleskeith.com>
2018-07-25 08:55:59 +0800

    get local image

--- lib/friends.dart
+++ lib/friends.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'image_picker_channel.dart';
+import 'dart:io';
 
 class FriendsList extends StatefulWidget {
 
@@ -11,62 +13,75 @@ class FriendsList extends StatefulWidget {
 
 class FriendsState extends State<FriendsList> {
 
+  ImagePicker _imagePicker = new ImagePickerChannel();
+
+  File _imageFile;
+
+  void captureImage(ImageSource captureMode) async {
+    try {
+      var imageFile = await _imagePicker.pickImage(imageSource: captureMode);
+      setState(() {
+        _imageFile = imageFile;
+      });
+    }
+    catch (e) {
+      print(e);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return new MaterialApp(
-      theme: new ThemeData(
-        
-      ),
-      home: new Scaffold(
-        appBar: new AppBar(
-          title: new Text("朋友圈"),
-          backgroundColor: Colors.grey[200],
-          actions: <Widget>[
-            new IconButton(
-                icon: new Icon(Icons.camera_alt, color: Colors.black,),
-                onPressed: () {
-                  showModalBottomSheet(
-                      context: context,
-                      builder: (BuildContext context) {
-                        return new Container(
-                          child: new Padding(
-                            padding: const EdgeInsets.all(10.0),
-                            child: new Column(
-                              children: <Widget>[
-                                new ListTile(
-                                  title: new Text('拍摄'),
-                                  subtitle: new Text('照片或视频'),
-                                  onTap: (){
-
-                                  },
-                                ),
-                                new ListTile(
-                                  title: new Text('从手机相册选择'),
-                                  onTap: (){
-
-                                  },
-                                ),
-                                new ListTile(
-                                  title: new Text('取消'),
-                                  onTap: (){
-
-                                  },
-                                ),
-                              ],
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("朋友圈"),
+        backgroundColor: Colors.grey[200],
+        actions: <Widget>[
+          new IconButton(
+            icon: new Icon(Icons.camera_alt, color: Colors.black,),
+            onPressed: () {
+              showModalBottomSheet(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return new Container(
+                      height: 204.0,
+                      child: new Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: new Column(
+                          children: <Widget>[
+                            new ListTile(
+                              title: new Center(child: new Text('拍摄', textAlign: TextAlign.center)),
+                              subtitle: new Center(child: new Text('照片或视频', textAlign: TextAlign.center)),
+                                onTap: () => captureImage(ImageSource.photos)
                             ),
-                          ),
-                        );
-                      }
-                  );
-                },
-            )
-          ],
-        ),
+                            new ListTile(
+                              title: new Center(child: new Text('从手机相册选择', textAlign: TextAlign.center)),
+                              onTap: () => captureImage(ImageSource.camera)
+                            ),
+                            new ListTile(
+                              title: new Center(child: new Text('取消', textAlign: TextAlign.center)),
+                              onTap: (){
 
-        body: new ListView(
+                              },
+                            ),
+                          ],
+                        ),
+                      ),
+                    );
+                  }
+              );
+            },
+          )
+        ],
+      ),
 
-        ),
+      body: new ListView(
+        children: <Widget>[
+          new Container(
+            height: 200.0,
+            child: new Image.network('https://www.baidu.com/img/bd_logo1.png?qua=high&where=super'),
+          )
+        ],
       ),
     );
   }
