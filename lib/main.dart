f537722758a61fd02d838c7efc1ad8585e0dd1ab
Leon <liang.li@charleskeith.com>
2018-08-27 08:29:24 +0800

    daily

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:path_provider/path_provider.dart';
-import 'dart:math';
-import 'dart:async';
-import 'dart:io';
+import 'package:flutter/foundation.dart';
 import 'app.dart';
 
+final ThemeData kIOSTheme = new ThemeData(
+  primarySwatch: Colors.orange,
+  primaryColor: Colors.black,
+  primaryColorBrightness: Brightness.light,
+);
+
+final ThemeData kAndroidTheme = new ThemeData(
+  primarySwatch: Colors.purple,
+  accentColor: Colors.orangeAccent[400],
+);
 
 void main() {
   runApp(new MyApp());
@@ -16,10 +23,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return new MaterialApp(
       title: 'Flutter Demo',
-      theme: new ThemeData(
-        primaryColor: Colors.black,
-        primaryColorBrightness: Brightness.light,
-      ),
+      theme: defaultTargetPlatform == TargetPlatform.iOS
+          ? kIOSTheme
+          : kAndroidTheme,
       home: new AppMain(title: 'Flutter Demo Home Page'),
     );
   }
